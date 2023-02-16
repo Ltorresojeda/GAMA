44eaa31db5a69c008fcb364fc63a6e150f7af5e2
katherineleyq <katherineleyq@gmail.com>
2023-02-16 10:27:31 -0800

    minor

--- Spatial_02_08.R
+++ Spatial_02_08.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(here)
 library(janitor)
+library(terra)
+library(sf)
+library(rgdal)
 
 # load in data
 kern <- read_csv(here("data", "usgs_monitoring_allchem_kern.csv")) %>%
@@ -17,3 +20,13 @@ tulare$county <- "TULARE"
 
 # join all data
 all_chemicals <- rbind(kern, madera, tulare)
+
+# rasterize data
+chemicals_sf <- st_as_sf(all_chemicals, 
+                   coords = c('gm_longitude', 'gm_latitude'),
+                   crs = 4326,
+                   remove = F)
+
+chemicals_st <- st_transform(chemicals_sf, crs = 3338)
+
+plot(chemicals_st["gm_well_id"])
