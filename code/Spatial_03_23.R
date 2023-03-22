579d2575c74a24c780d961374ed6631c6dfb98e4
katherineleyq <katherineleyq@gmail.com>
2023-03-22 16:12:03 -0700

    data wrangling and rasterizing

--- code/Spatial_03_23.R
+++ code/Spatial_03_23.R
@@ -14,3 +14,27 @@ library(echarts4r) # even more interactive plots
 
 # read in wrangled data
 source("code/wrangling.R")
+
+# filter out kern county out of CA county shapefiles
+kern_shp <- CA_counties %>%
+  filter(NAME == "Kern") %>%
+  select("geometry")
+plot(kern_shp)
+
+# rasterize data from kern ammonia (or turn in sf object)
+kern_ammonia_sf <- st_as_sf(kern_ammonia,
+                    coords = c('gm_longitude', 'gm_latitude'),
+                    crs = 4326,
+                    remove = F)
+
+kern_ammonia_st <- st_transform(kern_ammonia_sf, crs = 4326)
+kern_shp_3338 <- st_transform(kern_shp, crs = 4326)
+
+
+well_id_joined <- st_join(kern_ammonia_st, kern_shp_3338,join = st_within)
+plot(well_id_joined["gm_result"])
+
+
+
+##### interactive plot
+
--- code/wrangling.R
+++ code/wrangling.R
@@ -13,3 +13,17 @@ tulare <- read_csv(here("data", "usgs_monitoring_tulare.csv")) %>%
 
 # join all data
 all_chemicals <- rbind(kern, madera, tulare)
+
+# load shape file
+CA_counties <- read_sf("~/Desktop/data/gama/CA_Counties/CA_Counties_TIGER2016.shp")
+
+# parse out Kern + Ammonia for plotting
+kern_ammonia <- kern %>%
+  filter(gm_chemical_name == "Ammonia") %>%
+  select(county, gm_well_id, gm_chemical_name, gm_result, gm_result_units,
+         gm_samp_collection_date, gm_latitude, gm_longitude)
+
+# transform gm_samp_collection_date from character to date
+kern_ammonia$gm_samp_collection_date <- as.Date(kern_ammonia$gm_samp_collection_date, format ="%m/%d/%y")
+
+
